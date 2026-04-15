f60857a4fd7a6895488bd1a5fcf37811456281c6
userl1l1 <usermabel16@gmail.com>
2026-04-15 14:01:59 -0300

    clase CamionAutonomo finalizado

--- src/CamionAutonomo.java
+++ src/CamionAutonomo.java
@@ -1,10 +1,18 @@
 
-public class CamionAutonomo extends Vehiculo {
+public class CamionAutonomo extends Vehiculo implements IConectable {
 
+	public CamionAutonomo(int id) {
+		super(id);
+	}
+	
 	@Override
 	public void patronMovimiento() {
-		// TODO Auto-generated method stub
-
+		System.out.println("Camion ID "+ getId()+ ": desplazandose por rutaterrestre");
 	}
-
+	
+	@Override
+	public void sincronizarGPS() {
+		System.out.println("Camion ID "+getId()+ ": sincronizando ruta con satelite logistico...");
+	}
+	
 }
